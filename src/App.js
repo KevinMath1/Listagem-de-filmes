76a1caacc216c321e47807dd0f7e89916743d20d
KevinMath1 <kevinmathias21@gmail.com>
2023-06-12 10:10:23 -0300

    primeiro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  const editItem = (index) => {
+    const newItemValue = prompt('Digite o novo texto:');
+    if (newItemValue.trim() !== '') {
+      const newItems = [...items];
+      newItems[index] = newItemValue.trim();
+      setItems(newItems);
+    }
+  };
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    
+    <div className="container">
+      <h1>Minha Lista de Filmes e Séries</h1>
+
+      <div id="lista">
+        <ul id="items">
+          {items.map((item, index) => (
+            <li key={index}>
+              <span>{item}</span>
+              <button onClick={() => editItem(index)}>Editar</button>
+              <button onClick={() => deleteItem(index)}>Excluir</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div id="cadastro">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Digite o nome do filme ou série"
+        />
+        <button onClick={addItem}>Adicionar</button>
+      </div>
     </div>
+   
   );
 }
 
